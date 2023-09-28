9a1f22a4cbc8c026f7da26ed4dd31957d4c4fb5f
Juan Castro <juan941223@gmail.com>
2023-09-28 17:23:25 -0500

    paso fronts a apigetway

---
+++ Escritorio/api/apiGateway/frontWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace frontWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Escritorio/api/apiGateway/frontWeb/Controllers/personaController.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace frontWeb.Controllers
+{
+    public class personaController : Controller
+    {
+        // GET: persona
+        public async Task<ActionResult> Inicio()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+               
+            }
+
+        }
+    }
+}
